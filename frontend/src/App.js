b14ffbe5fcecc0a4ce123b19d4999ddf56f2c91e
Tharik <mohamedkktharik@gmail.com>
2025-06-09 14:04:04 +0530

    Added files

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './components/home/Home'
+
+function App() {
+  return (
+    <div className="App">
+        <Home />
+        <h1>Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/home/Home.js
@@ -0,0 +1,11 @@
+import ResponsiveAppBar from "../navbar/Navbar";
+import React from "react";
+function Home(){
+    return(
+        <div>
+            <ResponsiveAppBar/>
+        </div>
+        
+    );
+}
+export default Home;
